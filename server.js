58873f463235a6916681450938680fb2b489fe63
Je Yun <alwaysblue21@gmail.com>
2023-06-19 14:48:27 -0400

    pokedex

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const PORT = process.env.PORT
 const app = express() 
 // const pokemon  = require('./models/pokemon.js');
 const pokemon = require("./models/pokemon.js");
+const methodOverride = require("method-override")
 
 // app.use(morgan("dev"))
-// app.use(express.static("public"))
+app.use(express.static("public"))
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(methodOverride("_method"))
+
 
 
 // routes
@@ -18,20 +24,44 @@ app.get("/pokemon", (req, res) => {
     });
 
 // new
-
+app.get("/pokemon/new", (req, res) => {
+    res.render("new.ejs")
+})
 //destory
 
 // update
+app.put("/pokemon/:id", (req, res) => {
+    const id = req.params.id
+    pokemon[id] = req.body
+    res.redirect("/pokemon")
+})
 
 //create
-
+app.post("/pokemon", (req, res) => {
+  let newPokemon = {
+    name: req.body.name,
+    img: req.body.image,
+  }  
+  newPokemon.stats = {
+    hp: req.body.hp,
+    attack: req.body.attack,
+    defense: req.body.defense
+  }
+  pokemon.push(req.body)
+  res.redirect("/pokemon")
+})
 //edit
+app.get("/pokemon/:id/edit", (req, res) => {
+    const id = req.params.id
+    const pokemonInfo = pokemon[id]
+    res.render("edit.ejs", {pokemonInfo, id})
+})
 
 //show
 app.get("/pokemon/:id", (req, res) => {
     const id = req.params.id
     const pokemonInfo = pokemon[id]
-    res.render("show.ejs", {pokemon, pokemonInfo, id});
+    res.render("show.ejs", {pokemonInfo, id});
     });
 
 
